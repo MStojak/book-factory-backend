ca7b4df4f109221025782480202ce59711358612
mstojak <mstojak@gzaop.local>
2023-05-03 16:44:13 +0200

    Initial commit

---
+++ src/main/java/hr/studio_moderna/book_factory/BookFactoryApplication.java
@@ -0,0 +1,13 @@
+package hr.studio_moderna.book_factory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookFactoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hr/studio_moderna/book_factory/BookFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.studio_moderna.book_factory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookFactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
