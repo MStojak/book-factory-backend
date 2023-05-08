d395881426f79e2b81e821a32926f571c2a10034
mstojak <62314262+MStojak@users.noreply.github.com>
2023-05-08 07:26:41 +0200

    1 initial setup (#4)
    * Initial commit
    
    * Initial commit - renaming project and path
    
    ---------
    
    Co-authored-by: mstojak <mstojak@gzaop.local>

---
+++ src/test/java/hr/sm/book_factory/BookFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.sm.book_factory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookFactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
